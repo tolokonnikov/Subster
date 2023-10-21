6abb4fdbbd2bb47d0c854e2bfd3a9c975c81a8b1
tolokonnikov <tolokonnikov.albert@gmail.com>
2023-10-22 01:07:25 +0300

    init

---
+++ Gui/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстові файли (*.txt)|*.txt|Усі файли (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Отримайте вибраний файл і його шлях
+                string selectedFileName = openFileDialog.FileName;
+                string selectedFilePath = openFileDialog.InitialDirectory;
+
+                // Обробка вибраного файлу
+                // Наприклад, відкриття файлу та читання його вмісту
+            }
+        }
+    }
+}
